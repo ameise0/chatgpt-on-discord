70ebba29a55364350e67e02c9c3774044de81065
ameise <ameise998244353@gmail.com>
2023-03-05 17:08:30 +0900

    dotenvを使用するように変更

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import os 
 import openai
 import discord
+from dotenv import load_dotenv
+
+load_dotenv()
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 discord_token = os.getenv("DISCORD_TOKEN_KEY")
